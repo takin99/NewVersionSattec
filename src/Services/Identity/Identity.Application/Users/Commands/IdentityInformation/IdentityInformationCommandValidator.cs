dfae4daef3c16d975b99c1b7442e59371a4cfbcb
Takin <117743366+takin99@users.noreply.github.com>
2022-11-27 13:25:04 +0330

    fixed validation for upload file

--- src/Services/Identity/Identity.Application/Users/Commands/IdentityInformation/IdentityInformationCommandValidator.cs
+++ src/Services/Identity/Identity.Application/Users/Commands/IdentityInformation/IdentityInformationCommandValidator.cs
@@ -1,11 +1,24 @@
-﻿using FluentValidation;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
 
 namespace sattec.Identity.Application.Users.Commands.IdentityInformation
 {
+    public class FileValidator : AbstractValidator<IFormFile>
+    {
+        public FileValidator()
+        {
+            RuleFor(x => x.Length).NotNull().LessThanOrEqualTo(1048576)
+                .WithMessage("File size is larger than allowed");
+
+            RuleFor(x => x.ContentType).NotNull().Must(x => x.Equals("image/jpeg") || x.Equals("image/jpg") || x.Equals("image/png"))
+                .WithMessage("format file is wrong!.. valid formats are (jpg, jpeg, png)");
+        }
+    }
     public class IdentityInformationCommandValidator : AbstractValidator<IdentityInformationCommand>
     {
         public IdentityInformationCommandValidator()
         {
+            RuleFor(x => x.NationalCardFile).SetValidator(new FileValidator());
 
             RuleFor(v => v.NationalId)
            .Length(10)
